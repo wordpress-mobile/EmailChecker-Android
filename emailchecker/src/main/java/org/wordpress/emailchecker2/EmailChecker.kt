5709e54d606def732d8c298e8bf50ea97aec1a2c
Maxime Biais <maxime.biais@gmail.com>
2016-03-29 11:39:37 +0200

    update domain list

--- emailchecker/src/main/java/org/wordpress/emailchecker2/EmailChecker.kt
+++ emailchecker/src/main/java/org/wordpress/emailchecker2/EmailChecker.kt
@@ -2,11 +2,49 @@ package org.wordpress.emailchecker2
 
 import java.util.*
 
+// https://github.com/mailcheck/mailcheck/wiki/List-of-Popular-Domains
 private val sModel =
-        setOf("yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com", "live.com",
-                "comcast.net", "comcast.com", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk",
-                "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com", "outlook.com",
-                "ymail.com")
+        sortedSetOf(
+                /* Default domains included */
+                "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
+                "google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "msn.com",
+                "live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk",
+
+                /* Other global domains */
+                "games.com" /* AOL */, "gmx.net", "hush.com", "hushmail.com", "icloud.com", "inbox.com",
+                "lavabit.com", "love.com" /* AOL */, "outlook.com", "pobox.com", "rocketmail.com" /* Yahoo */,
+                "safe-mail.net", "wow.com" /* AOL */, "ygm.com" /* AOL */, "ymail.com" /* Yahoo */, "zoho.com", "fastmail.fm",
+                "yandex.com",
+
+                /* United States ISP domains */
+                "bellsouth.net", "charter.net", "comcast.net", "cox.net", "earthlink.net", "juno.com",
+
+                /* British ISP domains */
+                "btinternet.com", "virginmedia.com", "blueyonder.co.uk", "freeserve.co.uk", "live.co.uk",
+                "ntlworld.com", "o2.co.uk", "orange.net", "sky.com", "talktalk.co.uk", "tiscali.co.uk",
+                "virgin.net", "wanadoo.co.uk", "bt.com",
+
+                /* Domains used in Asia */
+                "sina.com", "qq.com", "naver.com", "hanmail.net", "daum.net", "nate.com", "yahoo.co.jp", "yahoo.co.kr", "yahoo.co.id", "yahoo.co.in", "yahoo.com.sg", "yahoo.com.ph",
+
+                /* French ISP domains */
+                "hotmail.fr", "live.fr", "laposte.net", "yahoo.fr", "wanadoo.fr", "orange.fr", "gmx.fr", "sfr.fr", "neuf.fr", "free.fr",
+
+                /* German ISP domains */
+                "gmx.de", "hotmail.de", "live.de", "online.de", "t-online.de" /* T-Mobile */, "web.de", "yahoo.de",
+
+                /* Russian ISP domains */
+                "mail.ru", "rambler.ru", "yandex.ru", "ya.ru", "list.ru",
+
+                /* Belgian ISP domains */
+                "hotmail.be", "live.be", "skynet.be", "voo.be", "tvcablenet.be", "telenet.be",
+
+                /* Argentinian ISP domains */
+                "hotmail.com.ar", "live.com.ar", "yahoo.com.ar", "fibertel.com.ar", "speedy.com.ar", "arnet.com.ar",
+
+                /* Domains used in Mexico */
+                "hotmail.com", "gmail.com", "yahoo.com.mx", "live.com.mx", "yahoo.com", "hotmail.es", "live.com", "hotmail.com.mx", "prodigy.net.mx", "msn.com"
+        )
 
 fun suggestDomainCorrection(email: String): String {
     val splitEmail = email.split("@")
@@ -71,6 +109,11 @@ private fun edits(word: String): List<String> {
             edits.add(word.substring(0, i) + ('a' + c).toChar()  + word.substring(i))
         }
     }
+    // last char insert
+    for (c in 0..25) {
+        edits.add(word + ('a' + c).toChar())
+    }
+
     return edits
 }
 
